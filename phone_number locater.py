f4d899ef31134034d1faa978436cba8732df56b6
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2020-10-03 17:09:43 +0530

    Update phone_number locater.py

--- phone_number locater.py
+++ phone_number locater.py
@@ -2,14 +2,15 @@ import phonenumbers
 from phonenumbers import carrier
 from phonenumbers import geocoder
 from tabulate import tabulate
+# I also created a profile file to make this somewhat secure if you wanted to
 from profile import username, password
 
 
 def number_check(phone_number):
     number = phonenumbers.parse(phone_number)
-    description = geocoder.description_for_number(number, 'en')
+    country = geocoder.description_for_number(number, 'en')
     supplier = carrier.name_for_number(number, 'en')
-    info = [["Country", "Supplier"],[description, supplier]]
+    info = [["Country", "Supplier"],[country, supplier]]
 
     return tabulate(info, headers = "firstrow", tablefmt = "textile")
 
@@ -17,8 +18,6 @@ def number_check(phone_number):
 if __name__ == '__main__':
     uname = input("Enter username: ")
     pword = input("Enter password: ")
-    uname = 'Kavin'
-    pword = 'kavin20042205'
     
     if uname == username and pword == password:
         number = input("Enter number(Enter full phone number): ")
